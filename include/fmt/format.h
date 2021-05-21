osdeverr/fmt

commit 9a93a3d89a63aebbe5edf8ac7b21cb7c234c16bd

detail::write in one more place relevant to printf with long argument… (#2016)

include/fmt/format.h
@@ -605,6 +605,14 @@ OutputIt copy_str(InputIt begin, InputIt end, OutputIt it) {
   return it;
 }
 
+template <typename OutChar, typename InputIt,
+          FMT_ENABLE_IF(!needs_conversion<InputIt, OutChar>::value)>
+buffer_appender<OutChar> copy_str(InputIt begin, InputIt end,
+                                  buffer_appender<OutChar> out) {
+  get_container(out).append(begin, end);
+  return out;
+}
+
 template <typename Char, typename InputIt>
 inline counting_iterator copy_str(InputIt begin, InputIt end,
                                   counting_iterator it) {
@@ -2053,13 +2061,6 @@ OutputIt write(OutputIt out, basic_string_view<Char> value) {
   auto it = reserve(out, value.size());
   it = copy_str<Char>(value.begin(), value.end(), it);
   return base_iterator(out, it);
-}
-
-template <typename Char>
-buffer_appender<Char> write(buffer_appender<Char> out,
-                            basic_string_view<Char> value) {
-  get_container(out).append(value.begin(), value.end());
-  return out;
 }
 
 template <typename Char, typename OutputIt, typename T,

commit 886b7db518337a47854ab8b8bf397a6a5329a984

Fix binary size regression caused by 9a93a3d

include/fmt/format.h
@@ -459,6 +459,12 @@ buffer_appender<OutChar> copy_str(InputIt begin, InputIt end,
   return out;
 }
 
+template <typename OutChar, typename InputIt, typename OutputIt>
+FMT_NOINLINE OutputIt copy_str_noinline(InputIt begin, InputIt end,
+                                        OutputIt it) {
+  return copy_str<OutChar>(begin, end, it);
+}
+
 // A public domain branchless UTF-8 decoder by Christopher Wellons:
 // https://github.com/skeeto/branchless-utf8
 /* Decode the next character, c, from s, reporting errors in e.
@@ -1065,7 +1071,7 @@ inline format_decimal_result<Iterator> format_decimal(Iterator out, UInt value,
   // Buffer is large enough to hold all digits (digits10 + 1).
   Char buffer[digits10<UInt>() + 1];
   auto end = format_decimal(buffer, value, size).end;
-  return {out, detail::copy_str<Char>(buffer, end, out)};
+  return {out, detail::copy_str_noinline<Char>(buffer, end, out)};
 }
 
 template <unsigned BASE_BITS, typename Char, typename UInt>
@@ -1113,7 +1119,7 @@ inline It format_uint(It out, UInt value, int num_digits, bool upper = false) {
   // Buffer should be large enough to hold all digits (digits / BASE_BITS + 1).
   char buffer[num_bits<UInt>() / BASE_BITS + 1];
   format_uint<BASE_BITS>(buffer, value, num_digits, upper);
-  return detail::copy_str<Char>(buffer, buffer + num_digits, out);
+  return detail::copy_str_noinline<Char>(buffer, buffer + num_digits, out);
 }
 
 // A converter from UTF-8 to UTF-16.